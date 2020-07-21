626398c73ffd6c23155be4b5527835c9e72a6d52
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-07-21 13:36:18 -0700

    Initial_Setups

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+/////////////// Run application ///////////////////////
+window.addEventListener('load', () => {
+
+    window.alert("Hello World")
+    
+
+
+});
+
+
+
+// $('#deliveryAddress').change(function (e) {
+//     validateUserInput(e.target.id, e.target.value)
+//     console.log(e.target.id)
+// })
